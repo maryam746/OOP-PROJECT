8a33b78cdf6fc5729230123f894fc0653477c382
Maryam-Saleem <solivagantmaryam26@gmail.com>
2025-04-24 20:50:52 +0500

    Create Cubesolver.h

---
+++ Maryam/Cubesolver.h
@@ -0,0 +1,26 @@
+#ifndef CUBESOLVER_H
+#define CUBESOLVER_H
+
+#include "Cube.h"
+#include <vector>
+#include <string>
+#include <queue>
+#include <unordered_set>
+#include <map>
+
+using namespace std;
+
+class CubeSolver {
+public:
+    CubeSolver(const Cube& initialCube);
+    vector<string> solve(const Cube& currentCube);
+
+private:
+    Cube start;
+
+    int heuristic(const Cube& cube);
+    vector<string> getPossibleMoves();
+    Cube applyMove(const Cube& cube, const string& move);
+};
+
+#endif
