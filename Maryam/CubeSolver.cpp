885b65b79e79ac46b378a6e81a3556baf6d2d03d
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-05-11 14:39:04 +0500

    Update CubeSolver.cpp

--- Maryam/CubeSolver.cpp
+++ Maryam/CubeSolver.cpp
@@ -1,4 +1,8 @@
 #include "CubeSolver.h"
+#include <iostream>
+
+using namespace std;
+
 
 CubeSolver::CubeSolver(const Cube& initialCube) : start(initialCube) {}
 
@@ -23,19 +27,21 @@ vector<string> CubeSolver::getPossibleMoves() {
     }
     return moves;
 }
-
 Cube CubeSolver::applyMove(const Cube& cube, const string& move) {
     Cube newCube = cube;
     Cube::Rotation dir = (move[1] == '\'') ? Cube::Rotation::COUNTER_CLOCKWISE : Cube::Rotation::CLOCKWISE;
-    Cube::Face face;
+    Cube::Face face = Cube::Face::FRONT; // Initialize with a default value
 
     switch (move[0]) {
-        case 'F': face = Cube::Face::FRONT; break;
-        case 'B': face = Cube::Face::BACK; break;
-        case 'U': face = Cube::Face::UP; break;
-        case 'D': face = Cube::Face::DOWN; break;
-        case 'L': face = Cube::Face::LEFT; break;
-        case 'R': face = Cube::Face::RIGHT; break;
+    case 'F': face = Cube::Face::FRONT; break;
+    case 'B': face = Cube::Face::BACK; break;
+    case 'U': face = Cube::Face::UP; break;
+    case 'D': face = Cube::Face::DOWN; break;
+    case 'L': face = Cube::Face::LEFT; break;
+    case 'R': face = Cube::Face::RIGHT; break;
+    default:
+        cerr << "Invalid move: " << move << endl;
+        return newCube; // Return the cube unchanged for invalid moves
     }
 
     newCube.rotateFace(face, dir);
@@ -47,10 +53,28 @@ vector<string> CubeSolver::solve(const Cube& currentCube) {
     priority_queue<pair<int, pair<Cube, vector<string>>>, vector<pair<int, pair<Cube, vector<string>>>>, greater<>> pq;
     unordered_set<string> visited;
 
-    pq.push({heuristic(currentCube), {currentCube, {}}});
+    pq.push({ heuristic(currentCube), {currentCube, {}} });
 
     while (!pq.empty()) {
-        auto [cost, node] = pq.top(); pq.pop();
+        auto& [cost, node] = pq.top(); pq.pop();
+        while (!pq.empty()) {  
+           const auto& topElement = pq.top();
+           pq.pop();  
+           int cost = topElement.first;  
+           auto& node = topElement.second;
+           Cube cube = node.first;  
+           vector<string> path = node.second;  
+
+           if (cube.isSolved()) return path;  
+           if (path.size() >= 20) continue;  
+
+           for (auto& move : getPossibleMoves()) {  
+               Cube next = applyMove(cube, move);  
+               vector<string> nextPath = path;  
+               nextPath.push_back(move);  
+               pq.push({ heuristic(next) + static_cast<int>(nextPath.size()), {next, nextPath} });
+           }  
+        }
         Cube cube = node.first;
         vector<string> path = node.second;
 
@@ -61,7 +85,7 @@ vector<string> CubeSolver::solve(const Cube& currentCube) {
             Cube next = applyMove(cube, move);
             vector<string> nextPath = path;
             nextPath.push_back(move);
-            pq.push({heuristic(next) + nextPath.size(), {next, nextPath}});
+            pq.push({ heuristic(next) + static_cast<int>(nextPath.size()), {next, nextPath} });
         }
     }
 
