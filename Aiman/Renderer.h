c02ccd467ac62d2a3a36a7434f9b31cc1ba46eef
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-05-11 17:39:22 +0500

    Update Renderer.h

--- Aiman/Renderer.h
+++ Aiman/Renderer.h
@@ -15,6 +15,12 @@ public:
     void Init();                    // Initialize window and renderer
     void Draw(const Cube& cube);    // Draw the Rubik's Cube based on its current state
     void Close();                   // Close Window and cleanup
+	void UpdateCameraOrbit(); // Update camera position based on mouse movement
+	float Clamp(float value, float min, float max); // Clamp function to limit values
+    float yaw = 0.0f;
+    float pitch = 0.0f;
+    float cameraDistance = 10.0f; // Fixed distance from the cube
+    Vector3 target = { 0.0f, 0.0f, 0.0f }; // Always look at cube center
 
 private:
     void DrawCubie(int x, int y, int z, const Cube& cube);  // Draw a single small cube (cubie) at position with correct colored faces
