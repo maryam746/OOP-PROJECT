71664301b7d9c9f7df303252db2b31c2cdefc370
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-05-11 14:40:52 +0500

    Create main.cpp

---
+++ Umaima/main.cpp
@@ -0,0 +1,26 @@
+#include "Cube.h"
+#include "Renderer.h"
+#include "GUI.h"
+#include "CubeSolver.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Renderer renderer;
+    renderer.Init();
+
+    Cube cube;
+    CubeSolver solver(cube);
+    HintSystem hintSystem(&cube, &solver);
+    GUI gui(&cube, &solver, &hintSystem);
+
+
+    while (!WindowShouldClose()) {
+        gui.Update(cube, solver);
+        renderer.Draw(cube);
+        gui.Draw();
+    }
+
+    renderer.Close();
+    return 0;
+}
