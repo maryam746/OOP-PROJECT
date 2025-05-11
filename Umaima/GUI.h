2bc6197fa71f5f8360492e059df8b75cfc8e72bd
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-05-11 14:41:09 +0500

    Create GUI.h

---
+++ Umaima/GUI.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Cube.h"
+#include "CubeSolver.h"
+#include "Hint.h"
+#include <raylib.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class GUI {
+public:
+    GUI(Cube* cube, CubeSolver* solver, HintSystem* hintSystem);
+    void Update(Cube& cube, CubeSolver& solver);
+    void Draw();
+
+private:
+    Rectangle hintBtn, resetBtn, scrambleBtn, solveBtn;
+    void DrawButton(Rectangle rect, const char* text, Color bgColor);
+    HintSystem* hintSystem;
+    string statusMessage;
+
+    float messageTimer;
+};
